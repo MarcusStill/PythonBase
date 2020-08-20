cfec34ebc137dd3027a40efc8450e6c9ef6e7d5c
MarcusStill <belg.marcus@gmail.com>
2020-08-20 12:51:40 +0300

    Add: - test input - using list comprehensions --> changed functions

--- Lession3/3.py
+++ Lession3/3.py
@@ -1,11 +1,5 @@
 from time import time
 
-# rewrote
-def expon(numbers, e):
-    exp = [i ** e for i in range(len(numbers) + 1)]
-    exp = exp[1:]
-    return exp
-
 def prime(numbers):
     d = 2
     while d * d <= numbers and numbers % d != 0:
@@ -24,52 +18,58 @@ def time_decorators(func):
         return result
     return new_func
 
+
+def expon(numbers, e):
+    exp = [i ** e for i in range(len(numbers) + 1)]
+    exp = exp[1:]
+    return exp
+
+
 def test_numbers(number_list, var):
     array2 = []
     if var == 1:
-        for i in range(len(number_list)):
-            if int(number_list[i]) % 2 == 0:
-                array2.append(int(array[i]))
-        print(array2)
+        [array2.append(x) for x in range(len(number_list)) if x % 2 == 0]
+        array2 = array2[1:]
     elif var == 2:
-        for i in range(len(number_list)):
-            if int(number_list[i]) % 2 != 0:
-                array2.append(int(array[i]))
-        print(array2)
+        [array2.append(x) for x in range(len(number_list)) if x % 2 != 0]
     elif var == 3:
-        for i in range(len(number_list)):
-            if int(number_list[i]) != 1:
-                if prime(int(number_list[i])) == True:
-                    array2.append(int(array[i]))
-        print(array2)
+        [array2.append(number_list[x]) for x in range(len(number_list)) if (prime(number_list[x]) == True and number_list[x] > 1)]
+    return array2
+
+def test_input(number):
+    return number.isdigit()
+
 
 print('Home work lession 3')
-print('Select optio1n: \n 1 - Exponentation numbers, 2 - Test numbers')
-menu = int(input())
-if menu == 1:
-    print('Input list:')
-    array = input().split()
-    print('Input exp:')
-    e = int(input())
-    print('Start timer? \n 1 - Yes, 2 - No')
-    starttimer = int(input())
-    print('Exponentiation list:\n')
-    if starttimer == 1:
-        timer = time_decorators(expon)
-        print(timer(array, e))
+menu = str(input('Select option: \n 1 - Exponentation numbers, 2 - Test numbers\n'))
+if test_input(menu) == True:
+    if int(menu) == 1:
+        array = list(map(int, input('Input list: ').split()))
+        e = str(input('Input exp: '))
+        if test_input(e) == True:
+            e = int(e)
+            starttimer = int(input('Start timer? \n 1 - Yes, 2 - No\n'))
+            print('Exponentiation list:\n')
+            if starttimer == 1:
+                timer = time_decorators(expon)
+                print(timer(array, e))
+            else:
+                print(expon(array, e))
+        else:
+            print('Error input expon')
+    elif int(menu) == 2:
+        array = list(map(int, input('Input list: ').split()))
+        option = str(input('Select option: \n 1 - even, 2 - not even, 3 - prime\n'))
+        if test_input(option) == True:
+            starttimer = int(input('Start timer? \n 1 - Yes, 2 - No\n'))
+            if starttimer == 1:
+                timer = time_decorators(test_numbers)
+                print(timer(array, option))
+            else:
+                print(test_numbers(array, option))
+        else:
+            print('Error input option')
     else:
-        print(expon(array, e))
-elif menu == 2:
-    print('Input list:')
-    array = input().split()
-    print('Select option: \n 1 - even, 2 - not even, 3 - prime')
-    o = int(input())
-    print('Start timer? \n 1 - Yes, 2 - No')
-    starttimer = int(input())
-    if starttimer == 1:
-        timer = time_decorators(test_numbers)
-        timer(array, o)
-    else:
-        test_numbers(array, o)
-else    :
-    print('Error')
+        print('Error input menu')
+else:
+    print('Error input menu: only digit')

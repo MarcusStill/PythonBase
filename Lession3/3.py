db487cffff1e5a83e23f318b4c3d7db350ea5f70
MarcusStill <belg.marcus@gmail.com>
2020-08-25 17:57:16 +0300

    Add CONST + changed def prime

--- Lession3/3.py
+++ Lession3/3.py
@@ -1,10 +1,20 @@
 from time import time
 
+FILTER_EVEN = []
+FILTER_NOT_EVEN = []
+FILTER_PRIME = []
+
 def prime(numbers):
     d = 2
     while d * d <= numbers and numbers % d != 0:
         d += 1
     return d * d > numbers
+
+
+def prime_check(func, numbers):
+    prime_list = []
+    [prime_list.append(x) for x in array if (prime(x) == True and x > 1)]
+    return prime_list
 
 
 def time_decorators(func):
@@ -25,20 +35,19 @@ def expon(numbers, e):
 
 
 def test_numbers(number_list, var):
-    if var == 1:
+    if var == FILTER_EVEN:
         array2 = list(filter(lambda x: x % 2 == 0, number_list))
-    elif var == 2:
+    elif var == FILTER_NOT_EVEN:
         array2 = list(filter(lambda x: x % 2 != 0, number_list))
-    elif var == 3:
-        array2 = []
-        [array2.append(x) for x in number_list if (prime(x) == True and x > 1)]
+    elif var == FILTER_PRIME:
+        array2 = prime_check(prime, array)
     return array2
 
 
 print('Home work lession 3')
+array = list(map(int, input('Input list: ').split()))
 menu = int(input('Select optio1n: \n 1 - Exponentation numbers, 2 - Test numbers\n'))
 if menu == 1:
-    array = list(map(int, input('Input list: ').split()))
     e = int(input('Input exp: '))
     starttimer = int(input('Start timer? \n 1 - Yes, 2 - No\n'))
     print('Exponentiation list:\n')
@@ -49,13 +58,30 @@ if menu == 1:
         exp_list = [i ** e for i in array]
         print(exp_list)
 elif menu == 2:
-    array = list(map(int, input('Input list: ').split()))
     option = int(input('Select option: \n 1 - even, 2 - not even, 3 - prime\n'))
-    starttimer = int(input('Start timer? \n 1 - Yes, 2 - No\n'))
-    if starttimer == 1:
-        timer = time_decorators(test_numbers)
-        print(timer(array, option))
-    else:
-        print(test_numbers(array, option))
-else    :
-    print('Error')
+    if option == 1:
+        FILTER_EVEN = option
+        starttimer = int(input('Start timer? \n 1 - Yes, 2 - No\n'))
+        if starttimer == 1:
+            timer = time_decorators(test_numbers)
+            print(timer(array, FILTER_EVEN))
+        else:
+            print(test_numbers(array, FILTER_EVEN))
+    if option == 2:
+        FILTER_NOT_EVEN = option
+        starttimer = int(input('Start timer? \n 1 - Yes, 2 - No\n'))
+        if starttimer == 1:
+            timer = time_decorators(test_numbers)
+            print(timer(array, FILTER_NOT_EVEN))
+        else:
+            print(test_numbers(array, FILTER_NOT_EVEN))
+    if option == 3:
+        FILTER_PRIME = option
+        starttimer = int(input('Start timer? \n 1 - Yes, 2 - No\n'))
+        if starttimer == 1:
+            timer = time_decorators(test_numbers)
+            print(timer(array, FILTER_PRIME))
+        else:
+            print(test_numbers(array, FILTER_PRIME))
+else:
+    print('Menu item selection error!')

8fdd23b53f863b07a6f983189ddb7eb84fcb4cbe
MarcusStill <belg.marcus@gmail.com>
2020-10-13 00:23:00 +0300

    update charge/drive

--- Lesson6/bicycle.py
+++ Lesson6/bicycle.py
@@ -5,24 +5,7 @@ class Bicycle():
         self.model = model
         self.category = category
         self.wheels = wheels
-        self.odometr = 0
-
 
     def get_description(self):
-        full_name = f"{self.brand} {self.model}, категория {self.category}, диаметр колеса {self.wheels}"
-        return full_name.title()
-
-    def read_odometr(self):
-        print(f'Этот велосипед проехал {self.odometr} км.')
-
-    def update_odometr(self, km):
-        if km >= self.odometr:
-            self.odometr = km
-        else:
-            print('Ошибка ввода данных!')
-
-    def increment_odometr(self, km):
-        if km <= 0:
-            self.odometr +=km
-        else:
-            print('Ошибка ввода данных!')
+        specification = f"{self.brand} {self.model}, категория {self.category}, диаметр колеса {self.wheels}"
+        return specification.title()
--- Lesson6/electric_bicycle.py
+++ Lesson6/electric_bicycle.py
@@ -1,15 +1,51 @@
 from bicycle import Bicycle
 
-class ElectricBicycle(Bicycle):
-    def __init__(self, brand, model, category, wheels, motor, battery, mileage):
-        super().__init__(brand, model, category, wheels)
-        self.motor = motor
-        self.battery = battery
-        self.mileage = mileage
+class Battery():
 
-    def get_description(self):
-        full_name = f"{self.brand} {self.model}, категория {self.category}, диаметр колеса {self.wheels},  мотор {self.motor} Вт., {self.battery} Вт, Запас хода {self.mileage} км."
-        return full_name.title()
+    def __init__(self, battery_size=7):
+        self.battery_size = battery_size
 
     def describe_battery(self):
-        print(f"Этот велосипед имеет мотор можностью {self.motor} Вт.")
+        print(f"Этот велосипед имеет аккумулятор емкостью {self.battery_size} Ah.")
+
+    def get_range(self):
+        if self.battery_size == 7:
+            range = 25
+        elif self.battery_size == 10:
+            range = 40
+        print(f"Этот велосипед проедет примерно {range} km на полном заряде аккумулятора.")
+
+
+class ElectricBicycle(Bicycle):
+
+    def __init__(self, brand, model, category, wheels, motor, mileage):
+        Bicycle.__init__(self, brand, model, category, wheels)
+        self.motor = motor
+        self.mileage = 0
+        self.odometr = 0
+        self.battery = Battery()
+
+    def get_description(self):
+        specification = f"{self.brand} {self.model}, категория {self.category}, диаметр колеса {self.wheels},  мотор {self.motor} Вт, Запас хода {self.mileage} км."
+        return specification.title()
+
+    def read_odometr(self):
+        print(f'Этот велосипед проехал {self.odometr} км.')
+
+    def drive(self, km):
+        if self.mileage >= km:
+            self.odometr += km
+            self.mileage -= km
+        else:
+            print('Батарея разряжена. Зарядите аккумулятор!')
+
+    def charge_battery(self):
+        if self.battery.battery_size == 7:
+            self.mileage=25
+            print('Батарея заряжена!')
+        elif self.battery.battery_size == 10:
+            self.mileage=40
+            print('Батарея заряжена!')
+
+    def battery_info(self):
+        print(f'Запас хода {self.mileage} км.')

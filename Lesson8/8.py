3ddf4694272de4d05c2ecb0f39953981c07b5b9a
MarcusStill <belg.marcus@gmail.com>
2021-02-04 00:21:55 +0300

    add methods, fix bugs

--- Lesson8/8.py
+++ Lesson8/8.py
@@ -1,21 +1,129 @@
+from icecream import ic
 from datetime import datetime, date
 
+#ic.configureOutput(includeContext=True)
+
+class Child():
+
+    def __init__(self, first_name, last_name, birth_date, gender):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_date = birth_date
+        self.gender = gender
+        self.visit = 0
+
+    def get_description(self):
+        full_name = f"Информация о ребенке: {self.first_name} {self.last_name} {self.birth_date} {self.gender}"
+        return full_name.title()
+
+    def greeting(self):
+        print(f"Меня зовут {self.first_name} {self.last_name}.")
+
+    def calculate_age(self):
+        birth_date = datetime.strptime(self.birth_date, "%Y-%m-%d")
+        today = date.today()
+        age = today.year - birth_date.year
+        if today.month < birth_date.month or today.month == birth_date.month and today.day < birth_date.day:
+            age -= 1
+        print("Мне", age, "лет.")
+
+    def read_visit(self):
+        print(f"Этот ребенок был в детском центре {self.visit} раз.")
+
+    def update_visit(self, number):
+        self.visit = number
+
+
+class Parent(Child):
+
+    def __init__(self, first_name, last_name, birth_date, gender, phone, category):
+        super().__init__(first_name, last_name, birth_date, gender)
+        self.phone = phone
+        self.category = "-"
+
+    def get_description(self):
+        full_name = f"Информация о взрослом: {self.first_name} {self.last_name} {self.birth_date} {self.gender} №_тел {self.phone} категория {self.category}"
+        return full_name.title()
+
+    def update_visit(self):
+        print("Количество посещений детского центра родителями не ведется!")
+
+
+class Ticket():
+
+    def __init__(self, number, time_stamp, duration):
+        self.number = number
+        self.time_stamp = time_stamp
+        self.duration = duration
+
+    def get_description(self):
+        about_ticket = f"Билет №{self.number}, {self.time_stamp}, продолжительность {self.duration}"
+        return about_ticket.title()
+
+
+class TicketParent(Ticket, Parent):
+
+    def __init__(self, number, time_stamp, duration, first_name, last_name, category):
+        super().__init__(number, time_stamp, duration)
+        self.first_name = first_name
+        self.last_name = last_name
+        self.category = category
+
+    def get_description(self):
+        print_ticket = f"Билет №{self.number}, Дата {self.time_stamp}, Продолжительность {self.duration}, Посетитель {self.first_name} {self.last_name} Категория {self.category}"
+        return print_ticket.title()
+
+
+class TicketChild(Ticket, Parent):
+
+    def __init__(self, number, time_stamp, duration, first_name, last_name):
+        super().__init__(number, time_stamp, duration)
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def get_description(self):
+        print_ticket = f"Билет №{self.number}, Дата {self.time_stamp}, Продолжительность {self.duration}, Посетитель {self.first_name} {self.last_name}"
+        return print_ticket.title()
+
+
 def calculate_age(birth_date):
-    #birth_date = datetime.strptime(birth_date, "%d/%m/%Y")
     birth_date = datetime.strptime(birth_date, "%Y-%m-%d")
     today = date.today()
     age = today.year - birth_date.year
     if today.month < birth_date.month or today.month == birth_date.month and today.day < birth_date.day:
         age -= 1
-    #return print("Возраст: ", age)
     return age
+''' Если перенести функцию calculate_age внутрь класса Child, то к ней не получится обратиться следующим образом:
+    age = child_1.calculate_age(child_1.birth_date)
+    TypeError: calculate_age() takes 1 positional argument but 2 were given'''
 
 def check_age(func):
-    print('Ваш возраст', func, end='. ')
+    print("Возраст посетителя", func, end=" лет. ")
     if func <= 4:
-        print('Только экскурсия')
-    elif func <= 7:
-        print('Посещение, но с родителями')
+        print("Возможна только экскурсия по детскому центру в сопровождении родителей.")
+    elif func >= 5:
+        print("Возможно только посещение в сопровождении родителей.")
     elif func > 8:
-        print('Можно самому')
+        print("Возможно самостоятельное посещение.")
     return
+
+
+child_1 = Child("Иван", "Иванов", "2014-8-4", "муж")
+child_1.greeting()
+child_1.calculate_age()
+print(child_1.get_description())
+child_1.read_visit()
+check_age(calculate_age(child_1.birth_date)) #Проверка возможности посещения
+if (calculate_age(child_1.birth_date))>=4:
+    ticket_child_1 = TicketChild(1, datetime.now(), 1, child_1.first_name, child_1.last_name)
+    print(ticket_child_1.get_description())
+child_1.update_visit(1)
+child_1.read_visit()
+parent_1 = Parent("Дмитрий", "Иванов", "1980-1-13", "муж", "123654", "")
+parent_1.update_visit()
+print(parent_1.get_description())
+tick_parent_1 = TicketParent(1, datetime.now(), 1, parent_1.first_name, parent_1.last_name, parent_1.category)
+print(tick_parent_1.get_description())
+#print("Возраст ребенка ", calculate_age(child_1.birth_date), "лет.") #Проверяем возраст
+
+
--- Lesson8/test_check_age.py
+++ Lesson8/test_check_age.py
@@ -24,3 +24,18 @@ def check_age(func):
 res = check_age(calculate_age('2018-8-4'))
 res1 = check_age(calculate_age('2016-8-4'))
 res2 = check_age(calculate_age('2010-8-4'))
+
+posetitel = int(input('Введите количество посетителей: '))
+i = 1
+while posetitel >= i:
+    option = int(input('Выберите категорию посетителя: \n 1 - ребенок, 2 - взрослый\n'))
+    if option == 1:
+        nameVar = input('Type the name: ')
+        salVar = input('Type the salary: ')
+        dobVar = input('Type the doB: ')
+        titleVar = input('Type the title: ')
+
+        # Create the instance of the myStruct object
+        myInstance = myStruct(nameVar, salVar, dobVar, titleVar)
+
+    if option == 2:

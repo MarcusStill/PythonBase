3713323f2990d05a7cad3e198a994172900ec520
MarcusStill <belg.marcus@gmail.com>
2021-02-08 00:43:24 +0300

    + def create instance + menu + logics

--- Lesson8/8.py
+++ Lesson8/8.py
@@ -19,13 +19,9 @@ class Child():
     def greeting(self):
         print(f"Меня зовут {self.first_name} {self.last_name}.")
 
-    def calculate_age(self):
-        birth_date = datetime.strptime(self.birth_date, "%Y-%m-%d")
+    def calculate_age(birth_date):
         today = date.today()
-        age = today.year - birth_date.year
-        if today.month < birth_date.month or today.month == birth_date.month and today.day < birth_date.day:
-            age -= 1
-        print("Мне", age, "лет.")
+        return today.year - child_i.birth_date.year - ((today.month, today.day) < (child_i.birth_date.month, child_i.birth_date.day))
 
     def read_visit(self):
         print(f"Этот ребенок был в детском центре {self.visit} раз.")
@@ -57,7 +53,7 @@ class Ticket():
         self.duration = duration
 
     def get_description(self):
-        about_ticket = f"Билет №{self.number}, {self.time_stamp}, продолжительность {self.duration}"
+        about_ticket = f"Билет №{self.number}, {self.time_stamp}, продолжительность {self.duration} ч."
         return about_ticket.title()
 
 
@@ -70,8 +66,8 @@ class TicketParent(Ticket, Parent):
         self.category = category
 
     def get_description(self):
-        print_ticket = f"Билет №{self.number}, Дата {self.time_stamp}, Продолжительность {self.duration}, Посетитель {self.first_name} {self.last_name} Категория {self.category}"
-        return print_ticket.title()
+        about_ticket = f"Билет №{self.number}, Дата {self.time_stamp}, Продолжительность {self.duration} ч., Посетитель {self.first_name} {self.last_name} Льгота {self.category}"
+        return about_ticket.title()
 
 
 class TicketChild(Ticket, Parent):
@@ -82,48 +78,132 @@ class TicketChild(Ticket, Parent):
         self.last_name = last_name
 
     def get_description(self):
-        print_ticket = f"Билет №{self.number}, Дата {self.time_stamp}, Продолжительность {self.duration}, Посетитель {self.first_name} {self.last_name}"
-        return print_ticket.title()
+        about_ticket = f"Билет №{self.number}, Дата {self.time_stamp}, Продолжительность {self.duration} ч., Посетитель {self.first_name} {self.last_name}"
+        return about_ticket.title()
 
+class TicketExcursion(Ticket, Parent):
+    pass
 
-def calculate_age(birth_date):
-    birth_date = datetime.strptime(birth_date, "%Y-%m-%d")
-    today = date.today()
-    age = today.year - birth_date.year
-    if today.month < birth_date.month or today.month == birth_date.month and today.day < birth_date.day:
-        age -= 1
-    return age
+# def calculate_age(born):
+#     today = date.today()
+#     return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 ''' Если перенести функцию calculate_age внутрь класса Child, то к ней не получится обратиться следующим образом:
     age = child_1.calculate_age(child_1.birth_date)
     TypeError: calculate_age() takes 1 positional argument but 2 were given'''
 
-def check_age(func):
-    print("Возраст посетителя", func, end=" лет. ")
-    if func <= 4:
-        print("Возможна только экскурсия по детскому центру в сопровождении родителей.")
-    elif func >= 5:
-        print("Возможно только посещение в сопровождении родителей.")
-    elif func > 8:
-        print("Возможно самостоятельное посещение.")
-    return
+# def check_age(func):
+#     print("Возраст посетителя", func, end=" лет. ")
+#     if func <= 4:
+#         print("Возможна только экскурсия по детскому центру в сопровождении родителей.")
+#     elif func >= 5:
+#         print("Возможно только посещение в сопровождении родителей.")
+#     elif func > 8:
+#         print("Возможно самостоятельное посещение.")
+#     return
+
+def create_child_instance():
+    print("Введите данные старшего ребенка.")
+    first_name = input('Введите имя: ')
+    last_name = input('Введите фамилию: ')
+    date_entry = input('Введите дату в формате ГГГГ-ММ-ДД: ')
+    year, month, day = map(int, date_entry.split('-'))
+    birth_date = date(year, month, day)
+    gen = input('Выберите пол (1 - мужской, 2 - женский): ')
+    gender = ""
+    if gen == 1:
+        gender = 'муж'
+    if gen == 2:
+        gender = 'жен'
+    child = Child(first_name, last_name, birth_date, gender)
+    return child
+
+def create_parent_instance():
+    print("Введите данные взрослого.")
+    first_name = input('Введите имя: ')
+    last_name = input('Введите фамилию: ')
+    birth_date = datetime.strptime(input('Введите дату в формате ГГГГ-ММ-ДД: '), "%Y-%m-%d")
+    gen = input('Выберите пол (1 - мужской, 2 - женский): ')
+    gender = ""
+    if gen == 1:
+        gender = 'муж'
+    if gen == 2:
+        gender = 'жен'
+    phone = input('Введите номер телефона: ')
+
+    ''' WAF?! '''
+    cat = input('Вы имеете льготы? Если да, укажите их (1 - инвалид, 2 - многодетный): ')
+    if cat == 1:
+        category = "инвалид"
+    elif cat == 2:
+        category = "многодетный"
+    else:
+        category = ""
+    parent = Parent(first_name, last_name, birth_date, gender, phone, category)
+    return parent
+
+print("\nДобро пожаловать в детский центр!")
+i = 1
+j = 0
+companion = 0
+print('У нас возможна следующая родолжительность посещения: 1 час, 2 часа, 3 часа (безлимит).\n')
+duration = int(input('Выберите продолжительность посещения (1, 2 или 3): '))
+while j != 1:
+    pos = int(input('Выберите категорию посетителя (1 - ребенок, 2 - взрослый): '))
+    if pos == 1:
+        child_i = create_child_instance()
+        if child_i.calculate_age() >= 7:
+            print("Возможно самостоятельное посещение.")
+            ticket_child_i = TicketChild(i, datetime.now(), duration, child_i.first_name, child_i.last_name)
+            print(ticket_child_i.get_description())
+            companion = 1
+        elif 4 <= child_i.calculate_age() < 7:
+            if companion == 1:
+                print("Возможно посещение в сопровождении старшего ребенка.")
+                ticket_child_i = TicketChild(i, datetime.now(), duration, child_i.first_name, child_i.last_name)
+                print(ticket_child_i.get_description())
+            else:
+                print("Возможно только посещение в сопровождении родителей.")
+                parent_i = create_parent_instance()
+                tick_parent_i = TicketParent(i, datetime.now(), duration, parent_i.first_name, parent_i.last_name, parent_i.category)
+                print(tick_parent_i.get_description())
+                if parent_i.category != "":
+                    print('Вам положена скидка!')
+        else:
+            print("Возможна только экскурсия по детскому центру в сопровождении родителей.")
+            TicketExcursion
+    elif pos == 2:
+        if i == 1:
+            print("Вы на экскурсии")
+            TicketExcursion
+        else:
+            parent_i = create_parent_instance()
+            tick_parent_i = TicketParent(1, datetime.now(), duration, parent_i.first_name, parent_i.last_name,
+                                         parent_i.category)
+            print(tick_parent_i.get_description())
+    add = int(input('Добавить еще посетителя? \n 1 - Да, 2 - Нет\n'))
+    if add == 1:
+        i = i + 1
+    if add == 2:
+        print("Держите билеты. Надеемся что вам все понравится.")
+        j = 1
 
 
-child_1 = Child("Иван", "Иванов", "2014-8-4", "муж")
-child_1.greeting()
-child_1.calculate_age()
-print(child_1.get_description())
-child_1.read_visit()
-check_age(calculate_age(child_1.birth_date)) #Проверка возможности посещения
-if (calculate_age(child_1.birth_date))>=4:
-    ticket_child_1 = TicketChild(1, datetime.now(), 1, child_1.first_name, child_1.last_name)
-    print(ticket_child_1.get_description())
-child_1.update_visit(1)
-child_1.read_visit()
-parent_1 = Parent("Дмитрий", "Иванов", "1980-1-13", "муж", "123654", "")
-parent_1.update_visit()
-print(parent_1.get_description())
-tick_parent_1 = TicketParent(1, datetime.now(), 1, parent_1.first_name, parent_1.last_name, parent_1.category)
-print(tick_parent_1.get_description())
+
+# child_0 = Child("Иван", "Иванов", "2014-8-4", "муж")
+# child_0.greeting()
+# child_0.calculate_age()
+# print(child_0.get_description())
+# child_0.read_visit()
+# check_age(calculate_age(child_0.birth_date)) #Проверка возможности посещения
+# if (calculate_age(child_0.birth_date))>=4:
+#     ticket_child_1 = TicketChild(1, datetime.now(), 1, child_0.first_name, child_0.last_name)
+#     print(ticket_child_1.get_description())
+# child_0.update_visit(1)
+# child_0.read_visit()
+# parent_1 = Parent("Дмитрий", "Иванов", "1980-1-13", "муж", "123654", "")
+# parent_1.update_visit()
+# print(parent_1.get_description())
+# tick_parent_1 = TicketParent(1, datetime.now(), 1, parent_1.first_name, parent_1.last_name, parent_1.category)
+# print(tick_parent_1.get_description())
+
 #print("Возраст ребенка ", calculate_age(child_1.birth_date), "лет.") #Проверяем возраст
-
-
--- Lesson8/test_check_age.py
+++ Lesson8/test_check_age.py
@@ -1,41 +1,158 @@
-from datetime import datetime, date
+# from datetime import datetime, date
+#
+# def calculate_age(birth_date):
+#     #birth_date = datetime.strptime(birth_date, "%d/%m/%Y")
+#     birth_date = datetime.strptime(birth_date, "%Y-%m-%d")
+#     today = date.today()
+#     age = today.year - birth_date.year
+#     if today.month < birth_date.month or today.month == birth_date.month and today.day < birth_date.day:
+#         age -= 1
+#     #return print("Возраст: ", age)
+#     return age
+#
+# def check_age(func):
+#     print('Ваш возраст', func, end='. ')
+#     if func <= 4:
+#         print('Только экскурсия')
+#     elif func <= 7:
+#         print('Посещение, но с родителями')
+#     elif func > 8:
+#         print('Можно самому')
+#     return
+# class Child():
+#
+#     def __init__(self, first_name, last_name, birth_date, gender):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.birth_date = birth_date
+#         self.gender = gender
+#         self.visit = 0
+#
+# res = check_age(calculate_age('2018-8-4'))
+# res1 = check_age(calculate_age('2016-8-4'))
+# res2 = check_age(calculate_age('2010-8-4'))
+#
+# posetitel = int(input('Введите количество посетителей: '))
+# i = 1
+# while posetitel >= i:
+#     option = int(input('Выберите категорию посетителя: \n 1 - ребенок, 2 - взрослый\n'))
+#     if option == 1:
+#         nameVar = input('Type the name: ')
+#         salVar = input('Type the salary: ')
+#         dobVar = input('Type the doB: ')
+#         titleVar = input('Type the title: ')
+#
+#         # Create the instance of the myStruct object
+#         myInstance = myStruct(nameVar, salVar, dobVar, titleVar)
+#
+#     if option == 2:
 
-def calculate_age(birth_date):
-    #birth_date = datetime.strptime(birth_date, "%d/%m/%Y")
-    birth_date = datetime.strptime(birth_date, "%Y-%m-%d")
-    today = date.today()
-    age = today.year - birth_date.year
-    if today.month < birth_date.month or today.month == birth_date.month and today.day < birth_date.day:
-        age -= 1
-    #return print("Возраст: ", age)
-    return age
+''''''
+# from datetime import datetime, date
+# # date_entry = input('Enter a date in YYYY-DD format: \n')
+# # year, month, day = map(int, date_entry.split('-'))
+# # date1 = datetime.date(year, month, day)
+# def calculate_age(birth_date):
+#     birth_date = datetime.strptime(birth_date, "%Y-%m-%d")
+#     today = date.today()
+#     age = today.year - birth_date.year
+#     if today.month < birth_date.month or today.month == birth_date.month and today.day < birth_date.day:
+#         age -= 1
+#     return age
+#
+# #s = datetime.strptime(input('Enter a date in YYYY-DD format: '), "%Y-%m-%d")
+# s = '2018-04-29'
+# print(s)
+# print(calculate_age(s))
 
-def check_age(func):
-    print('Ваш возраст', func, end='. ')
-    if func <= 4:
-        print('Только экскурсия')
-    elif func <= 7:
-        print('Посещение, но с родителями')
-    elif func > 8:
-        print('Можно самому')
-    return
+# from datetime import datetime
+#
+# i = str(input('date: '))
+# try:
+#     dt_start = datetime.strptime(i, '%Y-%m-%d')
+#     print(dt_start)
+# except ValueError:
+#     print("Incorrect format")
+'''
+'''
+# from datetime import datetime, date
+#
+# # d = '2020-06-01'
+# # date_in_datetime = datetime.strptime(d, '%Y-%m-%d').date()
+# #
+# # print(date_in_datetime)
+#
+# # dt_start = str(input('date: '))
+# # dt_start = datetime.strptime(dt_start, '%Y-%m-%d')
+# # print(dt_start)
+#
+#
+# date_entry = input('Enter a date (i.e. 2017-7-1)')
+# year, month, day = map(int, date_entry.split('-'))
+# date = date(year, month, day)
+# print(date)
+# def calculate_age(birth_date):
+#     birth_date = datetime.strptime(birth_date, '%Y-%m-%d').date()
+#     today = date.today()
+#     age = today.year - birth_date.year
+#     if today.month < birth_date.month or today.month == birth_date.month and today.day < birth_date.day:
+#         age -= 1
+#     return age
+# print(calculate_age(date))
+# from datetime import datetime
+# from dataclasses import dataclass
+#
+#
+# @dataclass
+# class Human:
+#     first_name: str
+#     last_name: str
+#     patronymic: str
+#     date: datetime
+#
+#     def __post_init__(self):
+#         self.date = datetime.strptime(self.date, '%d-%m-%Y')
+#
+#     def __str__(self):
+#         return self.first_name + ' ' + self.last_name + ' ' + self.patronymic + ' ' \
+#                + datetime.strftime(self.date, '%d-%m-%Y')
+#
+#     @property
+#     def age(self):
+#         """Метод вычисления возраста по дате рождения"""
+#         return (datetime.now() - self.date).days / 365.25
+#
+#
+# humanOne = Human('Billy', 'Gibson', 'Hill', '20-02-1999')
+# humanTwo = Human('Stella', 'MacCol', 'Denny', '30-12-1995')
+# humanThree = Human('Kortney', 'MacCol', 'Denny', '15-09-1998')
+#
+# print(humanOne)
+# print(humanOne.age)
+# print(humanTwo)
+# print(humanTwo.age)
+# print(humanThree)
+# print(humanThree.age)
+from datetime import date
 
 
-res = check_age(calculate_age('2018-8-4'))
-res1 = check_age(calculate_age('2016-8-4'))
-res2 = check_age(calculate_age('2010-8-4'))
-
-posetitel = int(input('Введите количество посетителей: '))
-i = 1
-while posetitel >= i:
-    option = int(input('Выберите категорию посетителя: \n 1 - ребенок, 2 - взрослый\n'))
-    if option == 1:
-        nameVar = input('Type the name: ')
-        salVar = input('Type the salary: ')
-        dobVar = input('Type the doB: ')
-        titleVar = input('Type the title: ')
-
-        # Create the instance of the myStruct object
-        myInstance = myStruct(nameVar, salVar, dobVar, titleVar)
-
-    if option == 2:
+# def calculate_age(birth_date):
+#     today = date.today()
+#     age = today.year - birth_date.year
+#     full_year_passed = (today.month, today.day) < (birth_date.month, birth_date.day)
+#     if not full_year_passed:
+#         age -= 1
+#     return age
+# ''''''
+# from datetime import datetime, date
+#
+# def calculate_age(born):
+#     today = date.today()
+#     return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+#
+# date_entry = input('Enter a date (i.e. 2017-7-1)')
+# year, month, day = map(int, date_entry.split('-'))
+# date = date(year, month, day)
+# print(date)
+# print(type(date))
+# print(calculate_age(date))

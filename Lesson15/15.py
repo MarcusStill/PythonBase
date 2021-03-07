01c10f4aa3e5801f447483c7869e4e0b7565992d
MarcusStill <belg.marcus@gmail.com>
2021-03-07 13:10:12 +0300

    fix bug

--- Lesson15/15.py
+++ Lesson15/15.py
@@ -1,12 +1,11 @@
-# from db import *
 from api import *
-#Win10 Traceback (most recent call last)
-from functools import wraps
-from asyncio.proactor_events import _ProactorBasePipeTransport
-
-from sqlalchemy import create_engine, Column, Integer, String, MetaData, ForeignKey
-from sqlalchemy.orm import sessionmaker, scoped_session, relationship, session
+import datetime
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship
 from sqlalchemy.ext.declarative import declarative_base
+#from sqlalchemy.orm import session
+#from sqlalchemy import MetaData
+
 
 engine = create_engine("sqlite:///quote.db", echo=True)
 Base = declarative_base(bind=engine)
@@ -17,7 +16,7 @@ Session = scoped_session(session_factory)
 class Author(Base):
     __tablename__ = 'author'
     id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
-    name = Column(String(100), unique=True)
+    name = Column(String, nullable=True, unique=True)
 
 
 class Quote(Base):
@@ -25,114 +24,76 @@ class Quote(Base):
     id = Column(Integer, primary_key=True, nullable=False, unique=True)
     text = Column(String, nullable=True, unique=True)
     author_id = Column(Integer, ForeignKey('author.id'))
-    created_at = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
 
     author = relationship("Author", backref="quotes")
 
 
-def create_author(name: str) -> Author:
-    new_author = Author(name=name)
-    print("Id before", new_author.id)
-    session.add(new_author)
-    session.commit()
-    print("Id after", new_author.id)
-    return new_author
-
-
-def create_quote(text: str, author_id: int, created_at: str) -> Quote:
-    new_quote = Quote(text=text, author_id=author_id, created_at=created_at)
-    print("id before", new_quote.id)
-    session.add(new_quote)
-    session.commit()
-    print("id after", new_quote.id)
-    return new_quote
-
-
-
-#Win10 Traceback (most recent call last)
-def silence_event_loop_closed(func):
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        try:
-            return func(self, *args, **kwargs)
-        except RuntimeError as e:
-            if str(e) != 'Event loop is closed':
-                raise
-    return wrapper
-
-
 if __name__ == "__main__":
-    Base.metadata.create_all(engine)
-    print("Запрашиваем случайные цитаты с сайта favqs.com. Поиск останавливается когда номер цитаты без остатка делится на число 'n'.")
-    n = int(input('Введите число n: '))
-    k = 0
-    while k != 1:
-        quote = asyncio.run(get_quote())
-        id = int(quote['id'])
-        if id % n == 0:
-            k = 1
-            logger.info("Result. Id qoute: {}. Author: {}. Quote: {}", quote['id'], quote['author'], quote['body'])
-            #print(quote['id'])
-            search_quote = quote['id']
-            search_author = quote['author']
-            insert_quote = quote['body']
+    try:
+        print("Запрашиваем случайные цитаты с сайта favqs.com. Поиск останавливается когда номер цитаты без остатка делится на число 'n'.")
+        n = int(input('Введите число n: '))
+        k = 0
+        while k != 1:
+            quote = asyncio.run(get_quote())
+            id = int(quote['id'])
+            if id % n == 0:
+                k = 1
+                logger.info("Result. Id qoute: {}. Author: {}. Quote: {}", quote['id'], quote['author'], quote['body'])
+                search_id = quote['id']
+                search_author = quote['author']
+                search_quote = quote['body']
+    except:
+        print("Ошибка при обращении к api. Сервис недоступен.")
 
-
-    # Win10 Traceback (most recent call last)
-    _ProactorBasePipeTransport.__del__ = silence_event_loop_closed(_ProactorBasePipeTransport.__del__)
-
-
-    session = Session()
-    # # new_author = create_author("Bob Marley")
-    # print("Print all Author")
-    # for instance in session.query(Author).order_by(Author.id):
-    #     print("id", instance.id, instance.name)
-    # # new_quote = create_quote("Life is good", "1", "2021-03-02")
-    # # new_quote = create_quote("No Woman, no cry", "1", "2021-03-03")
-    #
-    # print("Print all quote one author")
-    # for instance in session.query(Quote).order_by(Quote.id):
-    #     print("id", instance.id, instance.text, "author_id", instance.author_id, instance.created_at)
-    #
-    # print("Print all quotes one Author")
-    # query = session.query(Author, Quote)
-    # query = query.join(Quote, Quote.author_id == Author.id)
-    # records = query.all()
-    # print(records)
-    # for author, quote in records:
-    #     print(author.name, "-", quote.text)
-    #
-    # """
-    # Логика
-    # """
-    result = 0
-    print("Ищем в БД цитату с id", search_quote)
-    for Quote in session.query(Quote.id).filter_by(id=search_quote):
-        result = 1
-    if not result:
-        print('Цитата с таким id в БД не найдена')
-        print("Ищем автора в БД", search_author)
-        for Quote in session.query(Author).filter_by(name=search_author):
-            result = 2
-        if result == 2:
-            print('Запрашиваемый автор в БД не найден')
-            #new_author = create_author(search_author)
-            new_author = create_author("12345")
-            #new_author = create_author(search_author)
-            #new_quote = create_quote(insert_quote, new_author.id, "2021-03-05")
-            #new_author.id
-        else:
-            print('Запрашиваемый автор в БД найден')
     else:
-        print("Цитата с таким id найдена в БД")
+        Base.metadata.create_all(engine)
+        session = Session()
 
-    session.close()
-
-    """
-    Добавление цитаты.
-        - поиск в БД по номеру. Если цитата с таким номером есть  -->  не вносим.
-        - если номера в БД нет --> Ищем автора. Если такой автор есть, то берем его номер и записываем цитату.
-        - если автора нет  --> вносим автора, берем его номер и записываем цитату.
-
-    """
-
+        """
+            Добавление цитаты.
+                - поиск в БД по номеру. Если цитата с таким номером есть  -->  не вносим.
+                - если номера в БД нет --> Ищем автора. Если такой автор есть, то берем его номер и записываем цитату.
+                - если автора нет  --> вносим автора, берем его номер и записываем цитату.
+    
+        """
+        print("search_author type", type(search_author))
+        result = 0
+        print("Ищем в БД цитату с id.", search_quote)
+        for Quote in session.query(Quote).filter_by(id=search_id):
+            result = 1
+            logger.info(result)
+        if not result:
+            logger.info('Цитата с таким id в БД не найдена.')
+            print('Цитата с таким id в БД не найдена.')
+            logger.info("Ищем автора в БД.", search_author)
+            print("Ищем автора в БД.", search_author)
+            for Quote in session.query(Author).filter_by(name=search_author):
+                result = 2
+            if result == 2:
+                logger.info('Запрашиваемый автор в БД не найден.')
+                print('Запрашиваемый автор в БД не найден.')
+                logger.info("Добавляем автора {}", search_author)
+                print("Добавляем автора", search_author)
+                add_author = Author(name=search_author)
+                session.add(add_author)
+                session.commit()
+                logger.info("Получаем его номер и записываем цитату.")
+                print("Получаем его номер и записываем цитату.")
+                new_author_id = session.query(Author).filter_by(name=search_author).first()
+                add_quote = Quote(text=search_quote, author_id=new_author_id)
+                session.add(add_quote)
+                session.commit()
+            else:
+                print('Запрашиваемый автор в БД найден. Получаем его номер и записываем цитату.')
+                logger.info('Запрашиваемый автор в БД найден. Получаем его номер и записываем цитату.')
+                new_author_id = session.query(Author).filter_by(name=search_author).first()
+                add_quote = Quote(text=search_quote, author_id=new_author_id)
+                session.add(add_quote)
+                session.commit()
+        else:
+            logger.info("Цитата с таким id найдена в БД.")
+            print("Цитата с таким id найдена в БД.")
+        session.close()
+    finally:
+        print("Программа завершила свою работу.")
--- Lesson15/api.py
+++ Lesson15/api.py
@@ -4,28 +4,11 @@ from aiohttp import ClientSession
 from loguru import logger
 
 
-#Win10 Traceback (most recent call last)
-from functools import wraps
-from asyncio.proactor_events import _ProactorBasePipeTransport
-
-
 @dataclass
 class Service:
     name: str
     url: str
     quote_field: str
-
-
-#Win10 Traceback (most recent call last)
-def silence_event_loop_closed(func):
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        try:
-            return func(self, *args, **kwargs)
-        except RuntimeError as e:
-            if str(e) != 'Event loop is closed':
-                raise
-    return wrapper
 
 
 async def fetch_json(session: ClientSession, url: str) -> dict:
@@ -34,7 +17,7 @@ async def fetch_json(session: ClientSession, url: str) -> dict:
     :param url:
     :return:
     """
-    async with session.get(url) as response:
+    async with session.get(url, ssl=False) as response:
         return await response.json()
 
 
@@ -66,19 +49,17 @@ async def get_quote():
 #     logger.info("Finish async main")
 
 
-def run_main():
-    print("Программа запрашивает случайные цитаты с сайта favqs.com. Поиск останавливается когда номер цитаты без остатка делится на число 'n'.")
-    n = int(input('Введите число n: '))
-    k = 0
-    while k != 1:
-        quote = asyncio.run(get_quote())
-        id = int(quote['id'])
-        if id % n == 0:
-            k = 1
-            logger.info("Result. Id qoute: {}. Author: {}. Quote: {}", quote['id'], quote['author'], quote['body'])
-    #Win10 Traceback (most recent call last)
-    _ProactorBasePipeTransport.__del__ = silence_event_loop_closed(_ProactorBasePipeTransport.__del__)
+# def run_main():
+#     print("Программа запрашивает случайные цитаты с сайта favqs.com. Поиск останавливается когда номер цитаты без остатка делится на число 'n'.")
+#     n = int(input('Введите число n: '))
+#     k = 0
+#     while k != 1:
+#         quote = asyncio.run(get_quote())
+#         id = int(quote['id'])
+#         if id % n == 0:
+#             k = 1
+#             logger.info("Result. Id qoute: {}. Author: {}. Quote: {}", quote['id'], quote['author'], quote['body'])
 
-if __name__ == "__main__":
-    run_main()
+# if __name__ == "__main__":
+#     run_main()
 

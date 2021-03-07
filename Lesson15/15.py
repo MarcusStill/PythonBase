db9bee227fb66c273e5a92f13c79ab5a59877cc7
MarcusStill <belg.marcus@gmail.com>
2021-03-08 00:16:04 +0300

    Code Cleaning

--- Lesson15/15.py
+++ Lesson15/15.py
@@ -1,17 +1,12 @@
 from api import *
 import datetime
+import asyncio
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import sessionmaker, scoped_session, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-# from sqlalchemy.orm import session
-# from sqlalchemy import MetaData
-#Win10 Traceback (most recent call last)
-from functools import wraps
-from asyncio.proactor_events import _ProactorBasePipeTransport
 
-
-engine = create_engine("sqlite:///quote_3.db", echo=False)
+engine = create_engine("sqlite:///quote3.db", echo=True)
 Base = declarative_base(bind=engine)
 session_factory = sessionmaker(bind=engine)
 Session = scoped_session(session_factory)
@@ -36,21 +31,9 @@ class Quote(Base):
     author_id = Column(Integer, ForeignKey('author.id'))
     tag_id = Column(Integer, ForeignKey('tags.id'))
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
-
     author = relationship("Author", backref="quotes")
     tags = relationship("Tags", backref="quotes")
 
-
-#Win10 Traceback (most recent call last)
-def silence_event_loop_closed(func):
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        try:
-            return func(self, *args, **kwargs)
-        except RuntimeError as e:
-            if str(e) != 'Event loop is closed':
-                raise
-    return wrapper
 
 def create_tag(description: str) -> Tags:
     add_tag = Tags(description=description)
@@ -65,16 +48,17 @@ def create_author(name: str) -> Author:
     session.commit()
     return add_author
 
+
 def create_quote(text: str, author_id: int, tag_id: int) -> Quote:
     add_quote = Quote(text=text, author_id=author_id, tag_id=tag_id)
     session.add(add_quote)
     session.commit()
     return add_quote
 
+
 if __name__ == "__main__":
     try:
-        print(
-        "Запрашиваем случайные цитаты с сайта favqs.com. Поиск останавливается когда номер цитаты без остатка делится на число 'n'.")
+        print("Запрашиваем случайные цитаты с сайта favqs.com. Поиск останавливается когда номер цитаты без остатка делится на число 'n'.")
         n = int(input('Введите число n: '))
         k = 0
         while k != 1:
@@ -82,31 +66,20 @@ if __name__ == "__main__":
             id = int(quote['id'])
             if id % n == 0:
                 k = 1
-                logger.info("Result. Id qoute: {}. Author: {}. Tags: {}. Quote: {}.", quote['id'], quote['author'], quote['tags'], quote['body'])
+                print("Результат. Id цитаты:", quote['id'], "Автор:", quote['author'], "Теги:", quote['tags'], "Текст:", quote['body'])
                 search_id = quote['id']
                 search_author = quote['author']
-                search_tag = quote['tags']
+                tag_list = quote['tags']
+                if not tag_list:
+                    tag_list = '-'
+                else:
+                    search_tag = (', '.join(tag_list))
                 search_quote = quote['body']
     except:
         print("Ошибка при обращении к api. Сервис недоступен.")
-
     else:
         Base.metadata.create_all(engine)
         session = Session()
-
-        # Win10 Traceback (most recent call last)
-        _ProactorBasePipeTransport.__del__ = silence_event_loop_closed(_ProactorBasePipeTransport.__del__)
-
-        # search_id = "1"
-        # search_author = "Bob Marley"
-        # search_tag = "music"
-        # search_quote = "No woman, no cry"
-
-        # search_id = "2"
-        # search_author = "Queen"
-        # search_tag = "music"
-        # search_quote = "We are the champion"
-
         print("Ищем в БД цитату с id", search_id)
         s_id = session.query(Quote).filter_by(id=search_id).first()
         if not s_id:
@@ -121,35 +94,22 @@ if __name__ == "__main__":
                     print("Tag не найден")
                     print("Записываем tag")
                     create_tag(search_tag)
-                    # add_tag = Tags(description=search_tag)
-                    # session.add(add_tag)
-                    # session.commit()
                 else:
                     print("Tag найден")
-                print("Записываем автора ---> и записываем цитату ---> и записываем tag")
-                # add_author = Author(name=search_author)
-                # session.add(add_author)
-                # session.commit()
+                print("Записываем автора")
                 create_author(search_author)
                 print("Получаем id автора, tag и записываем цитату")
                 s_author = session.query(Author).filter_by(name=search_author).first()
                 s_tag = session.query(Tags).filter_by(description=search_tag).first()
-                # add_quote = Quote(author_id=s_author.id, tag_id=s_tag.id, text=search_quote)
-                # session.add(add_quote)
-                # session.commit()
                 create_quote(search_quote, s_author.id, s_tag.id)
             else:
                 print("Автор найден")
                 print("Получаем id автора, tag и записываем цитату")
                 s_author = session.query(Author).filter_by(name=search_author).first()
                 s_tag = session.query(Tags).filter_by(description=search_tag).first()
-                # add_quote = Quote(author_id=s_author.id, tag_id=s_tag.id, text=search_quote)
-                # session.add(add_quote)
-                # session.commit()
                 create_quote(search_quote, s_author.id, s_tag.id)
         else:
             print("Цитата найдена. Ничего не делаем")
-
         session.close()
     finally:
         print("Программа завершила свою работу.")
--- Lesson15/api.py
+++ Lesson15/api.py
@@ -1,7 +1,5 @@
-import asyncio
 from dataclasses import dataclass
 from aiohttp import ClientSession
-from loguru import logger
 
 
 @dataclass
@@ -28,7 +26,6 @@ async def fetch_quote(service: Service) -> str:
     """
     async with ClientSession() as session:
         result = await fetch_json(session, service.url)
-    logger.info("Received quote: {}", result)
     return result[service.quote_field]
 
 
@@ -36,30 +33,3 @@ async def get_quote():
     service = Service("favqs.com", "https://favqs.com/api/qotd", "quote")
     my_quote = await fetch_quote(service)
     return my_quote
-
-
-# def run_main_async():
-#     logger.info("Start async main")
-#     coros = [
-#         get_quote(),
-#         get_quote(),
-#     ]
-#     task = asyncio.wait(coros)
-#     asyncio.run(task)
-#     logger.info("Finish async main")
-
-
-# def run_main():
-#     print("Программа запрашивает случайные цитаты с сайта favqs.com. Поиск останавливается когда номер цитаты без остатка делится на число 'n'.")
-#     n = int(input('Введите число n: '))
-#     k = 0
-#     while k != 1:
-#         quote = asyncio.run(get_quote())
-#         id = int(quote['id'])
-#         if id % n == 0:
-#             k = 1
-#             logger.info("Result. Id qoute: {}. Author: {}. Quote: {}", quote['id'], quote['author'], quote['body'])
-
-# if __name__ == "__main__":
-#     run_main()
-
